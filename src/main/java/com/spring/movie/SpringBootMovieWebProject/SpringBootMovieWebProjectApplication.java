8d1aa5f7b5f7b0df5ec2f7544ce8298f97aa677a
장재준 <43335818+gihohpkl12@users.noreply.github.com>
2022-07-12 19:50:29 +0900

    init

---
+++ src/main/java/com/spring/movie/SpringBootMovieWebProject/SpringBootMovieWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.movie.SpringBootMovieWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMovieWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/movie/SpringBootMovieWebProject/SpringBootMovieWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.movie.SpringBootMovieWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMovieWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
